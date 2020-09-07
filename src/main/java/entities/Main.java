80ef7188e06d963004512c469f0cbac4a3bd4096
Frederik Dahl <cph-fd76@cphbusiness.dk>
2020-09-07 15:55:07 +0200

    Create Main.java

---
+++ src/main/java/entities/Main.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import facades.MovieFacade;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import utils.EMF_Creator;
+
+/**
+ *
+ * @author Frederik Dahl <cph-fd76@cphbusiness.dk>
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory(); 
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.createNamedQuery("Movie.deleteAllRows").executeUpdate();
+            em.persist(new Movie("Batman begins", "Zack Snyder", 1));
+            em.persist(new Movie("Batman The Dark Knight", "Zack Snyder", 1));
+
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+
+    }
+}
