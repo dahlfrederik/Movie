4cd2eec3212a2639943aad02425af64c28968e1b
Frederik Dahl <cph-fd76@cphbusiness.dk>
2020-09-07 16:36:57 +0200

    Update Movie.java

--- src/main/java/entities/Movie.java
+++ src/main/java/entities/Movie.java
@@ -5,10 +5,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 
 @Entity
-@NamedQuery(name = "Movie.deleteAllRows", query = "DELETE from Movie")
+@NamedQueries({
+@NamedQuery(name = "Movie.deleteAllRows", query = "DELETE from Movie"),
+@NamedQuery(name = "Movie.getAll", query = "SELECT m from Movie m"),
+@NamedQuery(name = "Movie.getMovieById", query = "SELECT m from Movie m WHERE m.name LIKE :name"),
+})
 public class Movie implements Serializable {
 
     private static final long serialVersionUID = 1L;
