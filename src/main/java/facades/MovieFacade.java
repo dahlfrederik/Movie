e520dae48442f962cd999529f44b2d16b24063d0
Frederik Dahl <cph-fd76@cphbusiness.dk>
2020-09-08 15:10:03 +0200

    Update MovieFacade.java

--- src/main/java/facades/MovieFacade.java
+++ src/main/java/facades/MovieFacade.java
@@ -17,13 +17,13 @@ public class MovieFacade {
 
     private static MovieFacade instance;
     private static EntityManagerFactory emf;
-    
+
     //Private Constructor to ensure Singleton
-    private MovieFacade() {}
-    
-    
+    private MovieFacade() {
+    }
+
     /**
-     * 
+     *
      * @param _emf
      * @return an instance of this facade class.
      */
@@ -38,52 +38,83 @@ public class MovieFacade {
     private EntityManager getEntityManager() {
         return emf.createEntityManager();
     }
-    
-    public MovieDTO getMovieById(int id){
-         EntityManager em = emf.createEntityManager();
+
+    public void deleteAllMovies() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.createNamedQuery("Movie.deleteAllRows").executeUpdate();
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    public MovieDTO getMovieById(int id) {
+        EntityManager em = emf.createEntityManager();
         try {
             Query query2 = em.createNamedQuery("Movie.getMovieById");
             query2.setParameter("id", id);
             Movie mov = (Movie) query2.getSingleResult();
-            MovieDTO movDTO = new MovieDTO(mov); 
+            MovieDTO movDTO = new MovieDTO(mov);
             return movDTO;
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<MovieDTO> getAllMovies() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            Query query2 = em.createNamedQuery("Movie.getAll");
+
+            List<Movie> movieList = query2.getResultList();
+            List<MovieDTO> movieDTOList = new ArrayList();
+            for (Movie movie : movieList) {
+                MovieDTO movDTO = new MovieDTO(movie);
+                movieDTOList.add(movDTO);
+            }
+
+            return movieDTOList;
+        } finally {
+            em.close();
+        }
+    }
+
+    public MovieDTO getMovieByName(String name) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            Query query = em.createNamedQuery("Movie.getByTitle");
+            query.setParameter("name", name);
+            MovieDTO movDTO = (MovieDTO) query.getSingleResult();
+
+            return movDTO;
+        } finally {
+            em.close();
+        }
+    }
+
+    public long getMovieCount() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            long movieCount = (long) em.createQuery("SELECT COUNT(m) FROM Movie m").getSingleResult();
+            return movieCount;
         } finally {
             em.close();
         }
     }
     
-       public List<MovieDTO> getAllMovies() {
-         EntityManager em = emf.createEntityManager(); 
-        try{
-            Query query2 = em.createNamedQuery("Movie.getAll");
-                   
-            List<Movie> movieList = query2.getResultList(); 
-            List<MovieDTO> movieDTOList = new ArrayList(); 
-            for (Movie movie : movieList) {
-                MovieDTO movDTO = new MovieDTO(movie); 
-                movieDTOList.add(movDTO); 
-            }
-                     
-            return movieDTOList;
-        }
-        finally{
-            em.close(); 
-        }
-    }
-       
-       public MovieDTO getMovieByName(String name){
-              EntityManager em = emf.createEntityManager();
+      public void populateDB(){
+            EntityManager em = emf.createEntityManager();
         try {
-            Query query2 = em.createNamedQuery("Movie.getMovieByName");
-            query2.setParameter("name", name);
-            Movie mov = (Movie) query2.getSingleResult();
-            MovieDTO movDTO = new MovieDTO(mov); 
-            return movDTO;
+            em.getTransaction().begin();
+            em.persist(new Movie("Topgun", "Kelly McGills", 1986));
+            em.persist(new Movie("Kill Bill","John",2003));
+            em.persist(new Movie("Point Break", "John 2", 1991));
+            em.getTransaction().commit();
         } finally {
             em.close();
         }
     }
-           
-}
-  
 
+}
