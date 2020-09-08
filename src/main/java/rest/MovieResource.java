fc732c2d400d5744801e8966c01a79e93b72a345
Frederik Dahl <cph-fd76@cphbusiness.dk>
2020-09-08 15:54:05 +0200

    Pretty printing

--- src/main/java/rest/MovieResource.java
+++ src/main/java/rest/MovieResource.java
@@ -34,10 +34,10 @@ public class MovieResource {
             MovieFacade mf = MovieFacade.getFacadeExample(EMF); 
             MovieDTO mov = mf.getMovieById(id); 
                             
-            return new Gson().toJson(mov);
+            return GSON.toJson(mov);
         } catch (javax.persistence.NoResultException e) {
             String errorString = "The id " + id + " is not in the database and therefore this program cannot show you the result";
-            return new Gson().toJson(errorString);
+            return GSON.toJson(errorString);
         }
     }
         
@@ -49,10 +49,10 @@ public class MovieResource {
         try{
             MovieFacade mf = MovieFacade.getFacadeExample(EMF); 
             List<MovieDTO> movieList = mf.getAllMovies(); 
-            return new Gson().toJson(movieList);
+            return GSON.toJson(movieList);
         }catch (javax.persistence.NoResultException e) {
             String errorString = "The function is either not working or there might not be any data in the database. IT HAS BEEN CONTACTED";
-            return new Gson().toJson(errorString);
+            return GSON.toJson(errorString);
         }
        
     }
@@ -65,10 +65,10 @@ public class MovieResource {
             MovieFacade mf = MovieFacade.getFacadeExample(EMF); 
             MovieDTO mov = mf.getMovieByName(name);
                             
-            return new Gson().toJson(mov);
+            return GSON.toJson(mov);
         } catch (javax.persistence.NoResultException e) {
             String errorString = "The name " + name + " is not in the database and therefore this program cannot show you the result";
-            return new Gson().toJson(errorString);
+            return GSON.toJson(errorString);
         }
     }
     
@@ -80,10 +80,10 @@ public class MovieResource {
             MovieFacade mf = MovieFacade.getFacadeExample(EMF); 
             long count = mf.getMovieCount(); 
                             
-            return new Gson().toJson(count);
+            return GSON.toJson(count);
         } catch (javax.persistence.NoResultException e) {
             String errorString = "The count cannot be done in the database and therefore this program cannot show you the result";
-            return new Gson().toJson(errorString);
+            return GSON.toJson(errorString);
         }
     }
     
@@ -95,10 +95,10 @@ public class MovieResource {
             MovieFacade mf = MovieFacade.getFacadeExample(EMF); 
             mf.populateDB();
                             
-            return new Gson().toJson("The DB is populated, see /all for more");
+            return GSON.toJson("The DB is populated, see /all for more");
         } catch (javax.persistence.NoResultException e) {
             String errorString = "The count cannot be done in the database and therefore this program cannot show you the result";
-            return new Gson().toJson(errorString);
+            return GSON.toJson(errorString);
         }
     }
    
